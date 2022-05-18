60b0e3b9b80e434ee3cf8810de19deaa199a82c3
denrique-alvarez <denrique.alvarez@gmail.com>
2022-05-18 12:29:58 +0200

    setup app file and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import { Container } from './components/container-component/Container.jsx'
+import './App.css';
+import { Container } from './components/container/Container.jsx'
+import { Login } from './components/login/login';
 
 function App() {
   return (
-    <div className="App">
-      <Container />
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/chat/:id" element={<Container />} />
+            </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
